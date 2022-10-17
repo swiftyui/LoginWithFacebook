2fdae64634df886f8b396eaab8933e41b3ad3445
Arno van Zyl <vanzylarno@yahoo.com>
2022-10-17 10:26:31 +0200

    Initial Commit

---
+++ LoginWithFacebook/LoginWithFacebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginWithFacebookApp.swift
+//  LoginWithFacebook
+//
+//  Created by Arno van Zyl on 2022/10/17.
+//
+
+import SwiftUI
+
+@main
+struct LoginWithFacebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
